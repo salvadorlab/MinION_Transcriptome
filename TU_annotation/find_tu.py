0a46b42da910d3aad686afec79a815e6ca36fb0c
rx32940 <rx32940@uga.edu>
2022-03-09 13:19:58 -0500

    add tts feature

--- TU_annotation/find_tu.py
+++ TU_annotation/find_tu.py
@@ -19,7 +19,10 @@ from statistics import mode, multimode
 # parser.add_argument( "-rf",'--genereadfilter',metavar='',type=str,help='number of reads needed in a read cluster to keep the TSS, default is 2 reads', default=2)
 # args = parser.parse_args()
 
-
+# feature=args.feature
+# if !(feature == "TSS" | feature == "TTS"):
+#     print("feature to analyze must be either TSS or TTS\nCurrent feature is not recognized\nprogram exiting...\n")
+#     exit()
 # window=int(args.threshold)
 # gene_aware_cov_filter=int(args.genecovfilter)
 # gene_aware_count_filter=int(args.genereadfilter)
@@ -27,16 +30,20 @@ from statistics import mode, multimode
 
 # bam=args.bam
 # gff=args.gff
-# gene_output=os.path.join(args.output, "gene_aware.tab")
-# unaware_output=os.path.join(args.output, "unaware.tab")
+# sample=os.path.basename(bam).strip(".bam")
+# output_prefix=os.path.join(args.output,sample) if args.prefix == "" else os.path.join(args.output, args.prefix)
+# gene_output=output_prefix + "."+ feature +  ".gene_aware.tab"
+# unaware_output=output_prefix + "."+ feature +".unaware.tab" 
+# merged_output=output_prefix + "."+ feature  +  ".merged.tab"
 
 window=10
 gene_aware_cov_filter=0.3
 gene_aware_count_filter=2
+feature="TTS"
 
 bam="/scratch/rx32940/minION/polyA_directRNA/map/genome/bam/Q29_Copenhageni_Basecalled_May_22_2020_Direct-RNA_rna_filtered.linear.bam"
 gff="/scratch/rx32940/minION/polyA_cDNA/map/genome/reference/GCF_000007685.1_ASM768v1_genomic.gff"
-gene_output="/scratch/rx32940/minION/polyA_directRNA/TU_Annotation/input/Q29_Copenhageni_Basecalled_May_22_2020_Direct-RNA_rna_filtered.linear.gene_aware.tab"
+gene_output="/scratch/rx32940/minION/polyA_directRNA/TU_Annotation/input/Q29_Copenhageni_Basecalled_May_22_2020_Direct-RNA_rna_filtered.linear.gene_aware.tts。tab"
 unaware_output="/scratch/rx32940/minION/polyA_directRNA/TU_Annotation/input/Q29_Copenhageni_Basecalled_May_22_2020_Direct-RNA_rna_filtered.linear.unaware.tab"
 merged_output="/scratch/rx32940/minION/polyA_directRNA/TU_Annotation/input/Q29_Copenhageni_Basecalled_May_22_2020_Direct-RNA_rna_filtered.linear.merged.tab"
 
@@ -73,15 +80,21 @@ for chrom in chroms_list:
     all_reads[chrom]={}
     all_genes[chrom]={}
     pos_cov[chrom]={}
+    if feature == "TSS": # if detecting TTS, read end for + strand will be clustered, read start for - strand will be clustered
+        start=1
+        end=2
+    else: # if detecting tts, reads mapped to lead strand will be treated as reads mapped in the lagging strand in the tss analysis
+        start=2
+        end=1
     # return start and end of each read in a tuple
-    all_reads[chrom]["+"] = [(int(read.split('\t')[1]), int(read.split('\t')[2])) for read in bedlist if ("\t+" in read) == True & (chrom in read) == True]
+    all_reads[chrom]['+'] = [(int(read.split('\t')[start]), int(read.split('\t')[end])) for read in bedlist if ('\t+' in read) == True & (chrom in read) == True]
     # lagging strand has end site as start
-    all_reads[chrom]["-"] = [(int(read.split('\t')[2]), int(read.split('\t')[1])) for read in bedlist if ("\t-" in read) == True & (chrom in read) == True]
+    all_reads[chrom]['-'] = [(int(read.split('\t')[end]), int(read.split('\t')[start])) for read in bedlist if ('\t-' in read) == True & (chrom in read) == True]
     # initiate other dict
-    all_genes[chrom]["+"]=[]
-    all_genes[chrom]["-"]=[]
-    pos_cov[chrom]["+"]={}
-    pos_cov[chrom]["-"]={}
+    all_genes[chrom]['+']=[]
+    all_genes[chrom]['-']=[]
+    pos_cov[chrom]['+']={}
+    pos_cov[chrom]['-']={}
 
 # reference genome annotation
 genome_file=gff.strip(".gff") + ".genome.tab"
@@ -102,8 +115,12 @@ with open(gff) as g, open(genome_file, "w") as genome:
 
             
 
-def get_cluster_pos(read_list, strand):
-    if strand == "+":
+def get_cluster_pos(read_list, strand, feature):
+    if feature == "TTS":
+        cur_strand = "-" if strand == "+" else "+"
+    else:
+        cur_strand = strand
+    if cur_strand == "+":
         start_list=[x[0] for x in read_list]
         cluster_start = (min(multimode(start_list)) if start_list.count(mode(start_list)) > 1 else min(start_list)) + 1
         cluster_end = max([x[1] for x in read_list])
@@ -114,17 +131,21 @@ def get_cluster_pos(read_list, strand):
     
     return cluster_start, cluster_end
 
-# chrom="NC_005823.1"
-# strand="+"
-# gene=all_genes['NC_005823.1']['+'][1]
-def get_overlap_reads(gene,strand,cur_reads_list):
-    gene_start=gene[0] if strand == "+" else gene[1] # antisense initiate transcription from gene's 3' end
+chrom="NC_005823.1"
+strand="+"
+gene=all_genes['NC_005823.1']['+'][1]
+def get_overlap_reads(gene,strand,cur_reads_list,feature):
+    if feature == "TTS": # if tts, then gene end will be checked, if read start > gene end, then iteration will stop (for lead strand mapped read)
+        cur_strand="-" if strand == "+" else "+"
+    else:                    
+        cur_strand=strand
+    gene_start=gene[0] if cur_strand == "+" else gene[1] # antisense initiate transcription from gene's 3' end
     # get reads overlap with gene start site
     cur_read_index = 0
     overlap_read_index =[]
     # use to check if we can stop iterating the reads
-    read_start=0 if strand == "+" else 1  # always check the 5' end of the read 
-    read_end =1  if strand == "+" else 0
+    read_start=0 if cur_strand == "+" else 1  # always check the 5' end of the read 
+    read_end =1  if cur_strand == "+" else 0
     for read in cur_reads_list:
         # if sorted read start site is already larger than gene_start site, then stop the iterating mapped reads
         if read[read_start] > gene_start: 
@@ -178,10 +199,8 @@ with open(gene_output, "w") as go:
             all_reads[chrom][strand].sort() # sort all reads based on the start site (for lag strand, is end site)
             all_genes[chrom][strand].sort() # sort all genes based on the start site (for lag strand, is end site)
             for gene in all_genes[chrom][strand]:
-                gene_start=gene[0] if strand == "+" else gene[1] # antisense initiate transcription from gene's 3' end
-                gene_end=gene[1] if strand == "+" else gene[0]
                 gene_id=gene[2]
-                overlap_reads_index=get_overlap_reads(gene, strand, all_reads[chrom][strand])
+                overlap_reads_index=get_overlap_reads(gene, strand, all_reads[chrom][strand], feature)
                 total_overlapped_cov=len(overlap_reads_index)
                 # print(overlap_reads_index)
                 if total_overlapped_cov > 1:
@@ -194,7 +213,7 @@ with open(gene_output, "w") as go:
                             cur_cluster.append(read)
                             pre_start = read[0]
                         else:
-                            cluster_start, cluster_end = get_cluster_pos(cur_cluster, strand)
+                            cluster_start, cluster_end = get_cluster_pos(cur_cluster, strand, feature)
                             # print(len(cur_cluster))
                             if (len(cur_cluster) >= (total_overlapped_cov * gene_aware_cov_filter)) and len(cur_cluster) >= gene_aware_count_filter:
                                 gene_clusters[chrom][strand].append((cluster_start, cluster_end, gene_id, len(cur_cluster)))
@@ -202,7 +221,7 @@ with open(gene_output, "w") as go:
                             pre_start = read[0]
                             cur_cluster = [read]
                     # the last tss cluster when rest of the reads didn't get to go into the else condition to write their cluster into the df
-                    cluster_start, cluster_end = get_cluster_pos(cur_cluster, strand)
+                    cluster_start, cluster_end = get_cluster_pos(cur_cluster, strand, feature)
                     if (len(cur_cluster) >= (total_overlapped_cov * gene_aware_cov_filter)) and len(cur_cluster) >= gene_aware_count_filter:
                         # print("%s\t%d\t%d\t%s\t%d\t%s\n"%(chrom, cluster_start, cluster_end, gene_id, len(cur_cluster), strand))
                         gene_clusters[chrom][strand].append((cluster_start, cluster_end, gene_id, len(cur_cluster)))
@@ -259,14 +278,14 @@ with open(unaware_output, "w") as uo:
                 else: # if the start sites of the two reads are further than 10, save all the reads from previous loops into a cluster
                     # the cluster starts with the first read index recorded for this cluster, and end with the maximum end position for all the reads in this cluster, 
                     # also with size of the cluster
-                    cluster_start, cluster_end = get_cluster_pos(cur_cluster, strand)
+                    cluster_start, cluster_end = get_cluster_pos(cur_cluster, strand,feature)
                     if len(cur_cluster) > pos_cov[chrom][strand][cluster_start]:
                         a=uo.write("%s\t%d\t%d\t.\t%d\t%s\t%s\n"%(chrom,cluster_start, cluster_end,len(cur_cluster), strand, "filter=" + str(pos_cov[chrom][strand][cluster_start]))) 
                         all_unaware_clusters[chrom][strand].append((cluster_start, cluster_end,"unaware",len(cur_cluster)))
                     cur_cluster=[read] # end list reinitiated for the new cluster.
                     pre_start=start
                     cur_cluster_start_index = cur_read_index # the new clusters starts, from the current read 
-            cluster_start, cluster_end = get_cluster_pos(cur_cluster, strand)
+            cluster_start, cluster_end = get_cluster_pos(cur_cluster, strand,feature)
             if len(cur_cluster) > pos_cov[chrom][strand][cluster_start]:
                 all_unaware_clusters[chrom][strand].append((cluster_start, cluster_end,"unaware",len(cur_cluster)))
                 b=uo.write("%s\t%d\t%d\t.\t%d\t%s\t%s\n"%(chrom,cluster_start, cluster_end,len(cur_cluster), strand, "filter=" + str(pos_cov[chrom][strand][cluster_start]))) 
@@ -294,19 +313,13 @@ with open(merged_output, "w") as mo:
                     merged_end=max([pre_tss[1], tss[1]])
                     merged_gene=tss[2] if pre_tss[2] == "unaware" else pre_tss[2]
                     merged_cov=(tss[3]+pre_tss[3])/2
-                    merged_tss.append((merged_start, merged_end, merged_gene, merged_cov))
-                    a=mo.write("%s\t%d\t%d\t%s\t%d\t%s\n"%(chrom,merged_start, merged_end,merged_gene,merged_cov, strand))
                     pre_tss=(merged_start, merged_end, merged_gene, merged_cov)
-                    
                 else:
-                    merged_tss.append(tss)
-                    a=mo.write("%s\t%d\t%d\t%s\t%d\t%s\n"%(chrom,tss[0], tss[1],tss[2],tss[3], strand))
+                    merged_tss.append(pre_tss)
+                    a=mo.write("%s\t%d\t%d\t%s\t%d\t%s\n"%(chrom,pre_tss[0], pre_tss[1],pre_tss[2],pre_tss[3], strand))
                     pre_tss=tss 
             merged_clusters[chrom][strand]=merged_tss
             
-
-
-        
 
 """"
 TU identification:
